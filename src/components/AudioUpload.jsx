2484c78416aaf01dba00c69d1d58e2546c753159
morlen <lennart.moritz@iosb.fraunhofer.de>
2023-05-24 21:05:30 +0200

    add audio upload component

---
+++ src/components/AudioUpload.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import styled from "styled-components";
+
+function AudioUpload() {
+  const [uploadStatus, setUploadStatus] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleFileDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleFileUpload = async () => {
+    if (!selectedFile) {
+      setUploadStatus('No file selected');
+      return;
+    }
+
+    setUploadStatus('Uploading...');
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      setUploadStatus(`File uploaded at: ${data.file_save_location}`);
+    } catch (error) {
+      setUploadStatus('Upload failed');
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      <Card>
+        <p
+          style={{ padding: '0rem 1rem' }}
+        >
+          Audio
+        </p>
+        <DragDropArea
+          onDrop={handleFileDrop}
+          onDragOver={handleDragOver}
+        >
+          {selectedFile ? <p>Selected file: {selectedFile.name}</p> : <p>Drag and drop a MP3 or WAV file here, or</p>}
+          <input type="file" accept=".mp3,.wav" onChange={handleFileSelect} />
+        </DragDropArea>
+        <div>
+          <button onClick={handleFileUpload}>Upload</button>
+        </div>
+        {uploadStatus && <div>{uploadStatus}</div>}
+      </Card>
+
+      <Card>
+        {/* TODO: add transcript file upload handling */}
+        <p
+          style={{ padding: '0rem 1rem' }}
+        >
+          Transcript
+        </p>
+        <DragDropArea
+          onDrop={handleFileDrop}
+          onDragOver={handleDragOver}
+        >
+          {selectedFile ? <p>Selected file: {selectedFile.name}</p> : <p>Drag and drop a .txt file here, or</p>}
+          <input type="file" accept=".txt" onChange={handleFileSelect} />
+        </DragDropArea>
+        <div>
+          <button onClick={handleFileUpload}>Upload</button>
+        </div>
+        {uploadStatus && <div>{uploadStatus}</div>}
+      </Card>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 4rem 1rem;
+  flex-direction: row;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+`
+
+const Card = styled.div`
+  min-height: 14rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  background: gray;
+  width: 50%;
+
+  button {
+    color: #BF4F74;
+    font-size: 1em;
+    margin: 0.5em;
+    padding: 0.25em 1em;
+    border: 2px solid #BF4F74;
+    border-radius: 3px;
+  }
+`
+
+const DragDropArea = styled.div`
+  border: 1px dashed #ccc;
+  padding: 0.5rem;
+  margin-top: 1rem;
+  background: gray;
+
+  &:hover {
+    background: lightgray;
+  }
+`;
+
+export default AudioUpload;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import AudioUpload from "../components/AudioUpload";
+
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <AudioUpload />
+        </div>
+    )
+}
+
+export default Home;
